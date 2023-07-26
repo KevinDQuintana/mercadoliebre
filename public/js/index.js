836529d7d261c7992494ec54029af4e5c5171649
KevinDQuintana <kevinquintanaunjufi@gmail.com>
2023-07-26 12:42:19 -0300

    version responsive del proyecto

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+const menuButton = document.querySelector('.header__menu-button');
+const menu = document.querySelector('.header__menu');
+
+menuButton.addEventListener('click', () => {
+	let attrib = menu.getAttribute('data-visible');
+	if (attrib === 'false') {
+		menu.setAttribute('data-visible', true);
+	} else if (attrib === 'true') {
+		menu.setAttribute('data-visible', false);
+	}
+	console.log(menu.getAttribute('data-visible'));
+});
