8d264a55ada4d0b622138c67fac59598f014f7e3
KevinDQuintana <kevinquintanaunjufi@gmail.com>
2023-07-26 21:18:53 -0300

    arreglo el "package.json"

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const path = require('path');
-
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, '../', 'public')));
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'src', 'app.js'));
+	res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.listen(PORT, () => console.log(`server started in port: ${port}`));
+app.listen(PORT, () => console.log('Servidor Iniciado.'));
