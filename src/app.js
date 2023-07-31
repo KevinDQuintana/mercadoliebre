c90df5f3cf726ac3bd5fe42db143000421eab1ad
KevinDQuintana <kevinquintanaunjufi@gmail.com>
2023-07-31 16:34:51 -0300

    agrego el formulario de registro

--- src/app.js
+++ src/app.js
@@ -4,8 +4,14 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../', 'public')));
+
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/signin', function (req, res) {
+	// res.send('Funcionando');
+	res.sendFile(path.join(__dirname, 'views', 'signin.html'));
 });
 
 app.listen(PORT, () => console.log(`server started at port: ${PORT}`));
