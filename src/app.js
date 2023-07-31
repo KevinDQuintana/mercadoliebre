4943bb1177dcc68daea012c4088087192646468f
KevinDQuintana <kevinquintanaunjufi@gmail.com>
2023-07-31 17:06:43 -0300

    agrego el formulario de login

--- src/app.js
+++ src/app.js
@@ -9,9 +9,21 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.get('/signin', function (req, res) {
+app.get('/signup', function (req, res) {
 	// res.send('Funcionando');
+	res.sendFile(path.join(__dirname, 'views', 'signup.html'));
+});
+
+app.post('/signup', function (req, res) {
+	res.redirect('/');
+});
+
+app.get('/signin', function (req, res) {
 	res.sendFile(path.join(__dirname, 'views', 'signin.html'));
+});
+
+app.post('/signin', function (req, res) {
+	res.redirect('/');
 });
 
 app.listen(PORT, () => console.log(`server started at port: ${PORT}`));
